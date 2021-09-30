94a814f1a02986f383f90f740d3e5ff5caf0a4c1
Aaditya-Dhanraj <aaditya@payatu.io>
2021-10-01 02:07:13 +0530

    api key generated

--- src/Utils/BaseURL.js
+++ src/Utils/BaseURL.js
@@ -1,3 +1,4 @@
 export const BASE_URL_GITHUB_API = "https://api.github.com";
 
-export const AuthToken = "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";
+// export const AuthToken = "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";
+export const AuthToken = "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";
